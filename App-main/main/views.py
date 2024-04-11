3c58a71f3bbcf848ffcdc317b36f0841b2e508a5
iliya <qwerty9087650850@gmail.com>
2024-04-11 03:55:38 +0300

    second commit

--- App-main/main/views.py
+++ App-main/main/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'HOME',
+        'element': 'qsc',
+    }
+    
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse('home')
